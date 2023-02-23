da7eb9b39ae9f4431b90c8e2ba00a07d6f283037
iuriereabenchi <reabenchi91@ymail.com>
2023-02-23 12:22:57 +0200

    ex abstarctizarea

--- Test Automation Course./src/abstarctizarea/Main.java
+++ Test Automation Course./src/abstarctizarea/Main.java
@@ -5,12 +5,12 @@ public class Main {
 
         FormaGeometrica patrat1 = new Patrat(5) {
         };
-   patrat1.calculArie();
-        System.out.println("aria patratului este "+ patrat1.calculArie());
-      FormaGeometrica triunghi1= new Triunghi(5,9);
-      triunghi1.calculArie();
-        System.out.println("aria triunghiului este "+ triunghi1.calculArie());
-}
+        patrat1.calculArie();
+        System.out.println("aria patratului este " + patrat1.calculArie());
+        FormaGeometrica triunghi1 = new Triunghi(5, 9);
+        triunghi1.calculArie();
+        System.out.println("aria triunghiului este " + triunghi1.calculArie());
+    }
 
 }
 
