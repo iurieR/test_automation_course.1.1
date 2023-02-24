13ac9b338b3dd4d4d58bd52124fd2f990445f2fe
iuriereabenchi <reabenchi91@ymail.com>
2023-02-24 14:35:43 +0200

    topic3

---
+++ Test Automation Course./src/topic3/ExecuteOperations.java
@@ -0,0 +1,10 @@
+package topic3;
+
+public class ExecuteOperations {
+    public static void main(String[] args) {
+        Operations.printAritmeticOperations();
+        Operations.printLogicOperations();
+        Operations.printRelationarOperations();
+        Operations.useForAndWhile();
+    }
+}
