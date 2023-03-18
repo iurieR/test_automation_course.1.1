02050bc179d547b057148b9b46c33936154c8cc6
iuriereabenchi <reabenchi91@ymail.com>
2023-03-18 17:55:24 +0200

    topic8

---
+++ Test Automation Course./src/topic8/ServiciuStoc.java
@@ -0,0 +1,18 @@
+package topic8;
+
+public class ServiciuStoc {
+    public static void main(String[] args) {
+        Carte Nr1 = new Carte("Titlu1", "Autor1", 1, 100);
+        Carte Nr2 = new Carte("Titlu2", "Autor2", 2, 200);
+        Carte Nr3 = new Carte("Titlu3", "Autor3", 3, 300);
+        Carte Nr4 = new Carte("Titlu4", "Autor4", 4, 400);
+        Carte Nr5 = new Carte("Titlu5", "Autor5", 5, 500);
+
+        Stoc telecentru = new Stoc(1, "Miorita 1");
+        telecentru.adaugareCarteInStoc(Nr1);
+        telecentru.adaugareCarteInStoc(Nr2);
+        telecentru.adaugareCarteInStoc(Nr3);
+        System.out.println("la magazinul de la telecentru sunt :" + telecentru.numarulDeCartiInStoc());
+
+    }
+}
